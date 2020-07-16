1283d0b8ccf08b9a0d63d5c66d6ff2a49e4410b2
Stephenscts <63982900+Stephenscts@users.noreply.github.com>
2020-07-16 12:53:52 -0400

    Add files via upload

---
+++ numeric_e.py
@@ -0,0 +1,11 @@
+#Expresiones Numericas
+a=2
+print("el valor de a es ", a)
+a=a+2
+print("valor de a+2 =",a)
+b=a*17
+print("el valor de b es",b)
+c=b/7
+print("el valor de c es",c)
+d=c%5
+print("el resto de c dividido 5 es",d)
